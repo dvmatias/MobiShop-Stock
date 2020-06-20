7551dfaeb08128a47a6cfb9d6e6e143e2eaedd48
Matías <matias.delv.dom@gmail.com>
2020-06-20 19:59:19 -0300

    - Package refactor and code clean up.

--- feature-main/src/main/java/com/cmdv/feature/ui/MainActivity.kt
+++ feature-main/src/main/java/com/cmdv/feature/ui/MainActivity.kt
@@ -1,9 +1,10 @@
-package com.cmdv.feature
+package com.cmdv.feature.ui
 
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.cmdv.feature.R
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
--- feature-main/src/main/java/com/cmdv/feature/ui/fragments/StockListFragment.kt
+++ feature-main/src/main/java/com/cmdv/feature/ui/fragments/StockListFragment.kt
@@ -1,17 +1,16 @@
-package com.cmdv.feature
+package com.cmdv.feature.ui.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import androidx.navigation.fragment.findNavController
+import androidx.fragment.app.Fragment
+import com.cmdv.feature.R
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class FirstFragment : Fragment() {
+class StockListFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,8 +23,5 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<Button>(R.id.button_first).setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-        }
     }
 }
