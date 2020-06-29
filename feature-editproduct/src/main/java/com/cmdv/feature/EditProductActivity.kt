f5d96b32f7079240ba7b3c90a8b454c3bcafbb6e
Matías <matias.delv.dom@gmail.com>
2020-06-28 23:22:11 -0300

    - Added Edit Product Activity theme.

--- feature-editproduct/src/main/java/com/cmdv/feature/EditProductActivity.kt
+++ feature-editproduct/src/main/java/com/cmdv/feature/EditProductActivity.kt
@@ -2,10 +2,12 @@ package com.cmdv.feature
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_edit_product.*
 
 class EditProductActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_product)
+        setSupportActionBar(toolbar)
     }
 }
